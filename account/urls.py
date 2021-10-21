9e7aa36281b555a14f6a0587efba0ccf88611f0c
parsa shahmaleki <parsampsh@gmail.com>
2021-10-21 16:23:50 +0330

    account: add urls and raw views functions for all the needed actions

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
-    # ...
+    path('register/', views.register, name="account_register"),
+    path('get-token/', views.get_token, name="account_get_token"),
+    path('whoami/', views.whoami, name="account_whoami"),
+    path('reset-token/', views.reset_token, name="account_reset_token"),
+    path('reset-password/', views.reset_password, name="account_reset_password"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,1 +1,26 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def register(request):
+    """ Registers a new User """
+    return JsonResponse({"Hello": "register"})
+
+
+def get_token(request):
+    """ Returns token of the user by checking username and password (same as login) """
+    return JsonResponse({"Hello": "get_token"})
+
+
+def whoami(request):
+    """ Returns the user information by the token """
+    return JsonResponse({"Hello": "whoami"})
+
+
+def reset_token(request):
+    """ Resets the token of the user """
+    return JsonResponse({"Hello": "reset_token"})
+
+
+def reset_password(request):
+    """ Resets the user's password (same as forget password) """
+    return JsonResponse({"Hello": "reset_password"})
