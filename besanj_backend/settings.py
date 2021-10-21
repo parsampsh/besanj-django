de783e59a537ce1d874031255390e9c5d6eed1d0
parsa shahmaleki <parsampsh@gmail.com>
2021-10-21 15:42:57 +0330

    settings: use `account.apps.AccountConfig` instead of single `account` in the installed apps list

--- besanj_backend/settings.py
+++ besanj_backend/settings.py
@@ -34,7 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
