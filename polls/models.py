6b99370e52320061ebe92022a185850e30c5189c
parsa shahmaleki <parsampsh@gmail.com>
2021-10-23 19:50:51 +0330

    polls: some changes in Poll and Choice description field

--- polls/models.py
+++ polls/models.py
@@ -5,12 +5,13 @@ from django.contrib.auth.models import User
 class Poll(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000, null=True)
     is_published = models.BooleanField(default=False)
 
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000, null=True)
     sort = models.IntegerField(default=0)
     users = models.ManyToManyField(User)
