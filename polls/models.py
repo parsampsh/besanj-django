96864dd98ef10951eedf6a3aac1eaa935808c6e6
parsa shahmaleki <parsampsh@gmail.com>
2021-10-23 19:39:36 +0330

    polls: create models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Poll(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    is_published = models.BooleanField(default=False)
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    sort = models.IntegerField(default=0)
+    users = models.ManyToManyField(User)
