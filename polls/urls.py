1892e44d8eb749fb195a51cf9ce914994a01d2ff
parsa shahmaleki <parsampsh@gmail.com>
2021-10-23 20:28:48 +0330

    polls: views: add new views

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create, name="create_poll"),
+    path('update/', views.create, name="create_poll"),
+    path('delete/', views.create, name="create_poll"),
+    path('create_choice/', views.create, name="create_poll"),
+    path('update_choice/', views.create, name="create_poll"),
+    path('delete_choice/', views.create, name="create_poll"),
     path('choice/', views.choice, name="select_choice"),
     path('', views.index, name="polls_list"),
 ]
