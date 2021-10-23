72c44f5af360b0ec18d6c4be7a6895b965b904d7
parsa shahmaleki <parsampsh@gmail.com>
2021-10-23 19:46:28 +0330

    polls: add the models to the admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
