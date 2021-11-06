bf0ce8fda1320d453a95cf17daa75e3bceff0b6b
parsa shahmaleki <parsampsh@gmail.com>
2021-11-06 21:48:42 +0330

    comments: define Comment model (not completed)

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from polls.models import Poll
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    is_published = models.BooleanField(default=False)
+    text = models.CharField(max_length=500)
