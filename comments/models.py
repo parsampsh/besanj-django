4f8a70c30f498c842d677ef57738effde77a1ede
parsa shahmaleki <parsampsh@gmail.com>
2021-11-06 23:10:56 +0330

    comments: fix a problem in model, make migrations

--- comments/models.py
+++ comments/models.py
@@ -8,4 +8,4 @@ class Comment(models.Model):
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     is_published = models.BooleanField(default=False)
     text = models.CharField(max_length=500)
-    parent_comment = models.ForeignKey('self', blank=True)
+    parent_comment = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
