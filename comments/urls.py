f90699c21683bbbaf0d4d30caf17e549bd72371a
parsa shahmaleki <parsampsh@gmail.com>
2021-11-08 16:50:03 +0330

    comments: determine views and urls

--- comments/urls.py
+++ comments/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 
 urlpatterns = [
+    path('send/', views.send, name='send_comment'),
+    path('delete/', views.send, name='delete_comment'),
+    path('user_comments/', views.comments_by_user, name='comments_by_user'),
+    path('poll_comments/', views.comments_on_poll, name='comments_on_poll'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from account.views import require_token
 
-# Create your views here.
+
+@require_POST
+@require_token
+def send(request):
+    """ Send a new comment """
+    pass
+
+
+@require_token
+@require_POST
+def delete(request):
+    """ User delete their comment """
+    pass
+
+
+def comments_by_user(request):
+    """ List of comments by a spesific user """
+    pass
+
+
+def comments_on_poll(request):
+    """ List of comments on a spesific poll """
+    pass
