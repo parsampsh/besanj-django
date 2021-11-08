6a44ae214626f97a7150506605700374eba72e6f
parsa shahmaleki <parsampsh@gmail.com>
2021-11-08 16:42:45 +0330

    comments app: setup urls.py

--- besanj_backend/urls.py
+++ besanj_backend/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('polls/', include('polls.urls')),
+    path('comments/', include('comments.urls')),
 ]
