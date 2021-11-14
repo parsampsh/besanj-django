a6a8984f59db1c506595de4f3ecc5ccfa03b349a
parsa shahmaleki <parsampsh@gmail.com>
2021-11-14 13:04:05 +0330

    comments: create send comment API

--- comments/tests.py
+++ comments/tests.py
@@ -1,3 +1,94 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import *
+from account.models import Profile
 
-# Create your tests here.
+
+class TestComment(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.user1 = User(username="a", password='123')
+        self.user1.save()
+        self.user1.profile = Profile(api_token='1')
+        self.user1.profile.save()
+
+        self.user2 = User(username="b", password='123')
+        self.user2.save()
+        self.user2.profile = Profile(api_token='2')
+        self.user2.profile.save()
+
+        self.poll1 = Poll.objects.create(title='a', user=self.user1, description='a', is_published=True)
+        self.poll2 = Poll.objects.create(title='b', user=self.user1, description='b', is_published=True)
+        self.poll3 = Poll.objects.create(title='c', user=self.user1, description='c', is_published=False)
+
+        self.comment1 = Comment.objects.create(user=self.user2, poll=self.poll1, text="test", is_published=True)
+        self.comment2 = Comment.objects.create(user=self.user2, poll=self.poll1, text="hi", is_published=False)
+
+    def test_user_cannot_send_comment_without_authentication(self):
+        res = self.client.post('/comments/send/')
+        self.assertEquals(res.status_code, 401)
+
+        res = self.client.post('/comments/send/', HTTP_TOKEN='2')
+        self.assertEquals(res.status_code, 400)
+
+        res = self.client.post('/comments/send/', {'poll_id': 123}, HTTP_TOKEN='2')
+        self.assertEquals(res.status_code, 400)
+
+        res = self.client.post('/comments/send/', {'text': 'test'}, HTTP_TOKEN='2')
+        self.assertEquals(res.status_code, 400)
+
+    def test_comment_cannot_be_sent_on_wrong_poll(self):
+        res = self.client.post('/comments/send/', {'text': 'test', 'poll_id': 12345}, HTTP_TOKEN='2')
+        self.assertEquals(res.status_code, 404)
+
+        res = self.client.post('/comments/send/', {'text': 'test', 'poll_id': self.poll3.id}, HTTP_TOKEN='2')
+        self.assertEquals(res.status_code, 404)
+
+    def test_comment_cannot_be_sent_on_wrong_parent_comment(self):
+        res = self.client.post('/comments/send/', {
+            'text': 'test',
+            'poll_id': self.poll2.id,
+            'parent_comment_id': 12345
+        }, HTTP_TOKEN='2')
+        self.assertEquals(res.status_code, 404)
+
+        res = self.client.post('/comments/send/', {
+            'text': 'test',
+            'poll_id': self.poll2.id,
+            'parent_comment_id': self.comment2.id
+        }, HTTP_TOKEN='2')
+        self.assertEquals(res.status_code, 404)
+
+    def test_comment_can_be_sent(self):
+        res = self.client.post('/comments/send/', {
+            'text': 'created',
+            'poll_id': self.poll2.id,
+            'parent_comment_id': self.comment1.id
+        }, HTTP_TOKEN='2')
+        self.assertEquals(res.status_code, 201)
+        res_json = res.json()['created_comment']
+
+        created_comment = Comment.objects.filter(text='created', poll=self.poll2, parent_comment=self.comment1).get()
+        self.assertEquals(created_comment.user.id, self.user2.id)
+
+        self.assertEquals(res_json['id'], created_comment.id)
+        self.assertEquals(res_json['poll_id'], created_comment.poll.id)
+        self.assertEquals(res_json['parent_comment_id'], created_comment.parent_comment.id)
+        self.assertEquals(res_json['user']['email'], created_comment.user.email)
+        self.assertEquals(res_json['is_published'], created_comment.is_published)
+
+        res = self.client.post('/comments/send/', {
+            'text': 'sent',
+            'poll_id': self.poll1.id,
+        }, HTTP_TOKEN='1')
+        self.assertEquals(res.status_code, 201)
+        res_json = res.json()['created_comment']
+
+        created_comment = Comment.objects.filter(text='sent', poll=self.poll1, parent_comment=None).get()
+        self.assertEquals(created_comment.user.id, self.user1.id)
+
+        self.assertEquals(res_json['id'], created_comment.id)
+        self.assertEquals(res_json['poll_id'], created_comment.poll.id)
+        self.assertEquals(res_json['user']['email'], created_comment.user.email)
+        self.assertEquals(res_json['is_published'], created_comment.is_published)
+        self.assertTrue('parent_comment_id' not in res_json)
--- comments/views.py
+++ comments/views.py
@@ -1,13 +1,46 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from account.views import require_token
+from .models import *
 
 
 @require_POST
 @require_token
-def send(request):
+def send(request, user):
     """ Send a new comment """
-    pass
+    poll_id = request.POST.get('poll_id')
+    text = request.POST.get('text')
+    parent_comment_id = request.POST.get('parent_comment_id')
+
+    error_response = lambda err: JsonResponse({'error': err}, status=400)
+
+    if poll_id is None:
+        return error_response('field poll_id is required')
+
+    if text is None:
+        return error_response('field text is required')
+
+    try:
+        poll = Poll.objects.get(pk=int(poll_id))
+
+        if not poll.is_published:
+            return JsonResponse({'error': 'poll does not exists'}, status=404)
+    except:
+        return JsonResponse({'error': 'poll does not exists'}, status=404)
+
+    parent_comment = None
+    if parent_comment_id is not None:
+        try:
+            parent_comment = Comment.objects.get(pk=int(parent_comment_id))
+
+            if not parent_comment.is_published:
+                return JsonResponse({'error': 'comment does not exists'}, status=404)
+        except:
+            return JsonResponse({'error': 'comment does not exists'}, status=404)
+
+    comment = Comment.objects.create(user=user, poll=poll, text=text, parent_comment=parent_comment)
+
+    return JsonResponse({'created_comment': comment.to_json()}, status=201)
 
 
 @require_token
