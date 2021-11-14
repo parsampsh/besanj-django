2aef25f6035ecd68f6415665d965a0bc214a5e05
parsa shahmaleki <parsampsh@gmail.com>
2021-11-14 13:03:24 +0330

    comments: models: add method to_json to model Comment

--- comments/models.py
+++ comments/models.py
@@ -10,3 +10,18 @@ class Comment(models.Model):
     text = models.CharField(max_length=500)
     parent_comment = models.ForeignKey('self', blank=True, null=True, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def to_json(self):
+        output = {
+            'id': self.id,
+            'user': self.user.profile.to_json(),
+            'is_published': self.is_published,
+            'text': self.text,
+            'created_at': self.created_at,
+            'poll_id': self.poll.id
+        }
+
+        if self.parent_comment is not None:
+            output['parent_comment_id'] = self.parent_comment.id
+
+        return output
