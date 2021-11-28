72207c0d21b952a600914dfecdc46a91ef31c407
parsa shahmaleki <parsampsh@gmail.com>
2021-11-28 15:12:24 +0330

    CORS: add allowed headers

--- besanj_backend/settings.py
+++ besanj_backend/settings.py
@@ -146,3 +146,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+    "token",
+]
